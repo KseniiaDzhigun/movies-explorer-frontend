7e39b0d17253749cb0bf1e98e4c88026b0578eae
Kseniia Dzhigun <kseniia.balandina@gmail.com>
2023-02-23 21:12:16 +0100

    movies adaptive

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,40 +7,72 @@ const Navigation = ({ loggedIn }) => {
         <>
             {
                 loggedIn ? (
-                    <nav className="nav" >
-                        <ul className="nav__links nav__links_movie">
-                            <li>
-                                <NavLink
-                                    to="/movies"
-                                    className={({ isActive }) =>
-                                        isActive ? "nav__link nav__link_active nav__link_movie" : "nav__link nav__link_movie"
-                                    }
-                                >
-                                    Фильмы
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/saved-movies"
-                                    className={({ isActive }) =>
-                                        isActive ? "nav__link nav__link_active nav__link_movie" : "nav__link nav__link_movie"
-                                    }
-                                >
-                                    Сохранённые фильмы
-                                </NavLink>
-                            </li>
-                        </ul>
-                        <Link to="/movies" className="nav__link nav__link_account" >
-                            <img
-                                src={icon}
-                                alt="Иконка аккаунта пользователя"
-                                className="nav__icon"
-                            />
-                            Аккаунт
-                        </Link>
-                    </nav>
+                    <>
+                        <input class="nav__side-menu" type="checkbox" id="side-menu" />
+                        <label class="nav__hamb-menu" for="side-menu">
+                            <span class="nav__hamb-line">
+                            </span>
+                        </label>
+                        <nav className="nav nav__movies" >
+                            <ul className="nav__links nav__links_movie">
+                                <li className="nav__list-item">
+                                    <NavLink
+                                        to="/"
+                                        className={({ isActive }) =>
+                                            isActive ? "nav__link nav__link_active nav__link_movie" : "nav__link nav__link_movie"
+                                        }
+                                    >
+                                        Главная
+                                    </NavLink>
+                                </li>
+                                <li className="nav__list-item">
+                                    <NavLink
+                                        to="/movies"
+                                        className={({ isActive }) =>
+                                            isActive ? "nav__link nav__link_active nav__link_movie" : "nav__link nav__link_movie"
+                                        }
+                                    >
+                                        Фильмы
+                                    </NavLink>
+                                </li>
+                                <li className="nav__list-item">
+                                    <NavLink
+                                        to="/saved-movies"
+                                        className={({ isActive }) =>
+                                            isActive ? "nav__link nav__link_active nav__link_movie" : "nav__link nav__link_movie"
+                                        }
+                                    >
+                                        Сохранённые фильмы
+                                    </NavLink>
+                                </li>
+                                <li className="nav__list-item">
+                                    <NavLink
+                                        to="/profile"
+                                        className={({ isActive }) =>
+                                            isActive ? "nav__link nav__link_active nav__link_account" : "nav__link nav__link_account"
+                                        }
+                                    >
+                                        <img
+                                            src={icon}
+                                            alt="Иконка аккаунта пользователя"
+                                            className="nav__icon"
+                                        />
+                                        Аккаунт
+                                    </NavLink>
+                                </li>
+                            </ul>
+                            {/* <Link to="/movies" className="nav__link nav__link_account" >
+                                <img
+                                    src={icon}
+                                    alt="Иконка аккаунта пользователя"
+                                    className="nav__icon"
+                                />
+                                Аккаунт
+                            </Link> */}
+                        </nav>
+                    </>
                 ) : (
-                    <nav className="nav">
+                    <nav className="nav nav__main">
                         <ul className="nav__links nav__links_sign">
                             <li>
                                 <Link to="/signup" className="nav__link nav__link_main" >Регистрация</Link>
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -24,41 +24,41 @@ export const initialCards = [
         link: 'https://i.postimg.cc/wjfJZH37/film5.jpg',
         saved: false,
     },
-    {
-        name: 'Книготорговцы',
-        link: 'https://i.postimg.cc/WzvJg1Yf/film6.jpg',
-        saved: true,
-    },
-    {
-        name: 'Когда я думаю о Германии ночью',
-        link: 'https://i.postimg.cc/rm6d2pTF/film7.jpg',
-        saved: false,
-    },
-    {
-        name: 'Gimme Danger: История Игги и The Stooges',
-        link: 'https://i.postimg.cc/Bb4Xtwtr/film8.jpg',
-        saved: false,
-    },
-    {
-        name: 'Дженис: Маленькая девочка грустит',
-        link: 'https://i.postimg.cc/FH71fSsH/film9.jpg',
-        saved: false,
-    },
-    {
-        name: 'Соберись перед прыжком',
-        link: 'https://i.postimg.cc/MT8Grr0n/film10.jpg',
-        saved: false,
-    },
-    {
-        name: 'Пи Джей Харви: A dog called money',
-        link: 'https://i.postimg.cc/8cpzTJ0D/film11.jpg',
-        saved: false,
-    },
-    {
-        name: 'По волнам: Искусство звука в кино',
-        link: 'https://i.postimg.cc/hPGtwmm1/film12.jpg',
-        saved: false,
-    },
+    // {
+    //     name: 'Книготорговцы',
+    //     link: 'https://i.postimg.cc/WzvJg1Yf/film6.jpg',
+    //     saved: true,
+    // },
+    // {
+    //     name: 'Когда я думаю о Германии ночью',
+    //     link: 'https://i.postimg.cc/rm6d2pTF/film7.jpg',
+    //     saved: false,
+    // },
+    // {
+    //     name: 'Gimme Danger: История Игги и The Stooges',
+    //     link: 'https://i.postimg.cc/Bb4Xtwtr/film8.jpg',
+    //     saved: false,
+    // },
+    // {
+    //     name: 'Дженис: Маленькая девочка грустит',
+    //     link: 'https://i.postimg.cc/FH71fSsH/film9.jpg',
+    //     saved: false,
+    // },
+    // {
+    //     name: 'Соберись перед прыжком',
+    //     link: 'https://i.postimg.cc/MT8Grr0n/film10.jpg',
+    //     saved: false,
+    // },
+    // {
+    //     name: 'Пи Джей Харви: A dog called money',
+    //     link: 'https://i.postimg.cc/8cpzTJ0D/film11.jpg',
+    //     saved: false,
+    // },
+    // {
+    //     name: 'По волнам: Искусство звука в кино',
+    //     link: 'https://i.postimg.cc/hPGtwmm1/film12.jpg',
+    //     saved: false,
+    // },
     // {
     //     name: '33 слова о дизайне',
     //     link: 'https://i.postimg.cc/9FyJ64jz/film1.jpg',
