5f2d86422b1f8ab7059996d094cc199f5928de9f
Kseniia Dzhigun <kseniia.balandina@gmail.com>
2023-03-03 14:02:59 +0100

    feat: SearchForm validation

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,20 +1,43 @@
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
 import icon from '../../images/search-icon-grey.svg';
+import { useForm } from 'react-hook-form';
 import border from '../../images/search-vertical-border.svg';
+import { useEffect } from 'react';
+import { inputOptions } from '../../utils/Constants'
 
-const SearchForm = () => {
+const SearchForm = ({ film }) => {
+
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm();
+    const onSubmit = data => console.log(data);
+
+    useEffect(() => {
+        if (film) setValue(film);
+    }, [film]);
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
 
     return (
-        <form className="search__form" >
+        <form className="search__form" onSubmit={handleSubmit(onSubmit)}>
             <fieldset className="search__form-set">
                 <img
                     src={icon}
                     alt="Лупа"
                     className="search__icon"
                 />
-                <input id="film-input" placeholder="Фильм" type="text" className="search__input" required />
-                <span className="search__error"></span>
+                <div className="search__input-text">
+                <input
+                    id="film-input"
+                    name="film"
+                    {...register("film", { ...inputOptions.film, onChange: handleChange })}
+                    className={errors.film ? "search__input search__input_error" : "search__input"}
+                    placeholder="Фильм"
+                />
+                <p className="search__error">{errors.film ? errors.film.message : ''}</p>
+                </div>
                 <button type="submit" className="search__button-submit"></button>
             </fieldset>
             <img
